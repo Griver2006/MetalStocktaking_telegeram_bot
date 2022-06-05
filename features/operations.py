3d3f4279c0871d0d3ffd4a0c2f6dea6fa494253a
Griver <Ramzan.Elikhanov@gmail.com>
2022-06-05 19:09:17 +0300

    fixed an bug related to time zones again again

--- features/operations.py
+++ features/operations.py
@@ -20,7 +20,7 @@ async def do_plus_operation(message: Message):
         all_operations = AllOperations()
         date_time = str(message.date).split()  # Берём дату и время
         all_operations.date = datetime.datetime.strptime(date_time[0], "%Y-%m-%d").date()  # Записываем дату
-        all_operations.time = datetime.datetime.now(pytz.timezone('Russia/Moscow'))  # Записываем время
+        all_operations.time = datetime.datetime.strptime(date_time[1], "%H:%M:%S").time()  # Записываем время
         all_operations.metal = user.metal  # Записываем выбранный пользователем металл
         all_operations.quantity = float(split_message[0].replace(',', '.'))  # Записываем колличество металла
         # Проверяем, указал ли пользователь цену и если не указал, записываем по цене из metal_types -
@@ -55,7 +55,7 @@ async def do_plus_operation(message: Message):
                                                         f' Сумма: {round(all_operations.sum)}',
                                        reply_markup=inline_kb_markup)
         # Собираем данные для записи операции в google sheets
-        data = [date_time[0].replace('-', '.'), str(datetime.datetime.now(pytz.timezone('Russia/Moscow')))[:7],
+        data = [date_time[0].replace('-', '.'), str(datetime.datetime.now(pytz.timezone('europe/moscow'))),
                 all_operations.metal,
                 all_operations.quantity, all_operations.price, all_operations.sum, all_operations.comment]
         dbs.commit()
