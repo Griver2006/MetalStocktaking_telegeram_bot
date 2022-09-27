7d6b6c1812403eae2efb9bd99597018304b8aad7
Griver <Ramzan.Elikhanov@gmail.com>
2022-09-27 17:01:41 +0300

    updated packages and converted code to a paid server

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from aiogram import executor, types
 from aiogram.utils.executor import start_webhook
 
-from loader import dp, WEBHOOK_URL, WEBHOOK_PATH, WEBAPP_HOST, WEBAPP_PORT
+from loader import dp
 import handlers
 from utils.notify_users import for_startup
 import utils.set_default_commands
@@ -10,7 +10,6 @@ import utils.set_default_commands
 async def on_startup(dispatcher):
     await dp.bot.set_my_commands([
         types.BotCommand("update_prices", "Обновить цены")])
-    await dp.bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
     await for_startup(dispatcher)
 
 
@@ -19,12 +18,4 @@ async def on_shutdown(dispatcher):
 
 
 if __name__ == '__main__':
-    start_webhook(
-        dispatcher=dp,
-        webhook_path=WEBHOOK_PATH,
-        skip_updates=True,
-        on_startup=on_startup,
-        on_shutdown=on_shutdown,
-        host=WEBAPP_HOST,
-        port=WEBAPP_PORT,
-    )
+    executor.start_polling(dp, skip_updates=True, on_startup=for_startup)
--- loader.py
+++ loader.py
@@ -1,25 +1,16 @@
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from dotenv import load_dotenv
 import os
 
 from data import db_session
+
+load_dotenv()
 
 TOKEN = os.getenv('BOT_TOKEN')
 
 bot = Bot(token=TOKEN)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
-
-HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
-
-# webhook settings
-WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
-WEBHOOK_PATH = f'/webhook/{TOKEN}'
-WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
-
-# webserver settings
-WEBAPP_HOST = '0.0.0.0'
-WEBAPP_PORT = os.getenv('PORT', default=8000)
-
 
 db_session.global_init("db/Metals_with_data.db")
