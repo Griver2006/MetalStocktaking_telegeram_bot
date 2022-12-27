3fb29ab651a888179931b1495df5881c88d7a1e0
Griver <Ramzan.Elikhanov@gmail.com>
2022-12-27 20:08:11 +0300

    fix bug, in file "do_record.py", when you click on the "Веса вписаны" button without any data, the keyboard "reply_kb_metals" is returned to us, and it should be "reply_kb_kush"

--- keyboards/reply_kb_kush_recording.py
+++ keyboards/reply_kb_kush_recording.py
@@ -7,5 +7,5 @@ reply_kb_kush_recording = ReplyKeyboardMarkup(resize_keyboard=True)
 
 metal_types = dict(call_metals_prices())
 load_buttons(reply_kb_kush_recording, initial_buttons=['Веса вписаны', 'Сбросить общую сумму'],
-             two_buttons_row=list(metal_types.keys()), end_buttons=['Удалить последную запись',
+             two_buttons_row=list(metal_types.keys()), end_buttons=['Удалить последнюю запись',
                                                                     'Вернуться в меню куша'], btn_back=False)
