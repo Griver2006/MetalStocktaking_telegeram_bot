a9eff9e2a22cef63d0db62dfce73e02861c647f9
Griver <Ramzan.Elikhanov@gmail.com>
2023-04-27 21:15:24 +0300

    now when user reset_total_amount, metal always change on 'Черный'

--- features/reset_total_amount.py
+++ features/reset_total_amount.py
@@ -1,3 +1,5 @@
+import utils.some_variable
+
 from aiogram.types import Message
 
 from keyboards.inline_kb_markup import inline_kb_markup
@@ -10,6 +12,8 @@ async def reset_total_amount(message: Message):
     # Сбрасываем общую сумму клиента в базе
     dbs = db_session.create_session()
     user = dbs.query(User).get(message.from_user.id)
+    user.metal = 'Черный'
+    user.price = float(utils.some_variable.metal_types['Черный'])
     user.client_amount = 0
     dbs.commit()
     # И также сбрасываем это значение у кнопки (Здесь на самом деле я удаляю кнопку)
