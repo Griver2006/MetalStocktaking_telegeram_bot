27a70905e20bfa16d5062c53eeb0e00035509a1f
Griver <Ramzan.Elikhanov@gmail.com>
2023-08-30 13:01:13 +0300

    Now, when the bot turns on, all user values reset to default

--- utils/notify_users.py
+++ utils/notify_users.py
@@ -1,9 +1,20 @@
+from utils.some_variable import metal_types
+
 from keyboards.reply_kb_menu import reply_kb_menu
+
 from data import db_session
 from data.users import User
 
 
 async def for_startup(dp):
     # При запуске бота возвращяем всех пользователей в меню
-    for user_id in db_session.create_session().query(User.id).all():
-        await dp.bot.send_message(user_id[0], 'Бот запущен!', reply_markup=reply_kb_menu)
+    dbs = db_session.create_session()
+    for user in dbs.query(User).all():
+        user.metal = 'Черный'
+        user.price = float(metal_types['Черный'])
+        user.client_amount = 0
+        user.kush_recording = False
+        user.operation_ended = False
+        user.kush_percent = 0
+        await dp.bot.send_message(user.id, 'Бот запущен!', reply_markup=reply_kb_menu)
+    dbs.commit()
