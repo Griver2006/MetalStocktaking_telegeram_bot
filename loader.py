7c41aff567af90684e83d47830792a86ef468790
Yakub <alhazurovyakub000@gmail.com>
2024-09-01 14:29:34 +0300

    fixs

--- loader.py
+++ loader.py
@@ -8,6 +8,9 @@ from data import db_session
 load_dotenv()
 
 TOKEN = os.getenv('BOT_TOKEN')
+if not TOKEN:
+    print('Введите токен вашего бота в .env!')
+    exit()
 
 bot = Bot(token=TOKEN)
 storage = MemoryStorage()
